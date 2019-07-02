4e45b3acf518892d8cc077e8c2d7be4dc19e43de
GarciaDFE <garcia.dfe@gmail.com>
2019-07-02 18:21:11 -0300

    Refatorado Desafio avançado eliminando objeto Grid e desenhando Grid no background do objeto Table

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -3,14 +3,12 @@ import Table from "../../objects/Table"
 import PlayerX from "../../objects/PlayerX"
 import PlayerO from "../../objects/PlayerO"
 import PlayerEmpty from "../../objects/PlayerEmpty"
-import Grid from "../../objects/Grid"
 import "./styles.css"
 
 const Game = () => {
      return (
           <div className="game">
                <Table />
-               <Grid />
                <div className="players">
                     <PlayerEmpty />
                     <PlayerO />
