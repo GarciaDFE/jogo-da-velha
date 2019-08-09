afa27c1795462c5ec7dcf592204a425e2c3a161d
GarciaDFE <garcia.dfe@gmail.com>
2019-08-09 10:48:58 -0300

    [054] - Desafio básico

--- src/objects/PlayerGame/index.js
+++ src/objects/PlayerGame/index.js
@@ -1,11 +1,20 @@
 import React, { useState } from "react";
 import "./styles.css";
-
+let play = "x"
 
 const PlayerGame = ({player = "false"}) => {
      const [statePlayer, setStatePlayer] = useState(player);
-     
-     const handleClick = () => setStatePlayer("x")
+
+     const handleClick = () => {
+          if (play === "x") {
+               setStatePlayer("x")
+               play = "o";
+          } else {
+               setStatePlayer("o")
+               play = "x";
+          }
+     }
+
      return (
           <button onClick={handleClick} className={`player-game -${statePlayer}`}></button>
      )
